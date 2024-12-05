7b29499076bcec61c50f9d63eb59b2e1b648237e
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-12-04 21:22:25 -0500

    Update enqueue.php

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -2,12 +2,12 @@
 function vedbo_enqueue_assets() {
     // Styles
     wp_enqueue_style('theme-style', get_stylesheet_uri());
-    wp_enqueue_style('header-style', get_template_directory_uri() . '/components/header/header.css', [], '1.0.0');
+    wp_enqueue_style('header-style', get_stylesheet_directory_uri() . '/components/header/header.css', [], '1.0.0');
 
     // Scripts
     wp_enqueue_script(
         'header-script',
-        get_template_directory_uri() . '/components/header/header.js',
+        get_stylesheet_directory_uri() . '/components/header/header.js',
         ['jquery'],
         '1.0.0',
         true
@@ -17,7 +17,7 @@ function vedbo_enqueue_assets() {
     if (is_checkout()) {
         wp_enqueue_script(
             'checkout-ajax',
-            get_template_directory_uri() . '/js/ajax_checkout_update_order_review.js',
+            get_stylesheet_directory_uri() . '/js/ajax_checkout_update_order_review.js',
             ['jquery'],
             '1.0.0',
             true
@@ -25,13 +25,14 @@ function vedbo_enqueue_assets() {
 
         wp_localize_script(
             'checkout-ajax',
-            'ajax_var',
+            'ajax_checkout__update',
             [
                 'url'    => admin_url('admin-ajax.php'),
-                'nonce'  => wp_create_nonce('my-ajax-nonce'),
+                'nonce'  => wp_create_nonce('my-ajax-nonce-2'),
                 'action' => 'update_order_review',
             ]
         );
     }
 }
 add_action('wp_enqueue_scripts', 'vedbo_enqueue_assets');
+;
