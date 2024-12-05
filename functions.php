5905e65be3cdc3f387dddcc2d17bf2b55e32e1ca
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2024-12-04 21:22:20 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,18 +1,18 @@
 <?php
 // Load theme assets
-require_once get_template_directory() . '/inc/enqueue.php';
+require_once get_stylesheet_directory() . '/inc/enqueue.php';
 
 // WooCommerce customizations
-require_once get_template_directory() . '/inc/checkout.php';
-require_once get_template_directory() . '/inc/custom-fields.php';
+require_once get_stylesheet_directory() . '/inc/checkout.php';
+require_once get_stylesheet_directory() . '/inc/custom-fields.php';
 
 // AJAX handlers
-require_once get_template_directory() . '/inc/ajax-handlers.php';
+require_once get_stylesheet_directory() . '/inc/ajax-handlers.php';
 
 // Utility functions
-require_once get_template_directory() . '/inc/helpers.php';
+require_once get_stylesheet_directory() . '/inc/helpers.php';
 
 // Services
-foreach (glob(get_template_directory() . '/inc/services/*.php') as $service_file) {
+foreach (glob(get_stylesheet_directory() . '/inc/services/*.php') as $service_file) {
     require_once $service_file;
 }
