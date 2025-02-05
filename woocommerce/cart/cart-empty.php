0645a9e33429ee51579e9283e646fcc25851eee8
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-02-05 14:07:50 -0500

    Update cart-empty.php

--- woocommerce/cart/cart-empty.php
+++ woocommerce/cart/cart-empty.php
@@ -20,7 +20,10 @@ defined( 'ABSPATH' ) || exit;
 
 .woocommerce-cart-empty-container h1 {
     font-size: 32px;
-    color: #333;
+    font-size: 23px;
+    font-weight: regular;
+    letter-spacing: 2.5px;
+    font-family: "Helvetica", sans-serif;
 }
 
 .woocommerce-cart-empty-container img {
@@ -31,39 +34,73 @@ defined( 'ABSPATH' ) || exit;
 
 .woocommerce-cart-empty-container .return-to-shop .button {
     display: inline-block;
-    background-color: var(--color--pink);
-    color: #fff;
-	border: 1px solid var(--color-pink);
     padding: 10px 20px;
+    border-radius: 20px;
+
+    font-weight: 300;
+    font-size: 16px;
+    color: var(--color--white);
     text-decoration: none;
-    border-radius: 5px;
+    text-transform: lowercase;
+    letter-spacing: 1px;
+
+
+	border: 1px solid  var(--color--black);
+    background-color: var(--color--black);
+}
+
+.woocommerce-cart-empty-container .return-to-shop .button strong{
+    font-weight: 700;
 }
 
 .woocommerce-cart-empty-container .return-to-shop .button:hover {
-	background-color: #fff;
-	border: 1px solid #fff;
-    color: var(--color--pink);
-}
+    display: inline-block;
+    padding: 10px 20px;
+    border-radius: 20px;
 
+    font-weight: 300;
+    font-size: 16px;
+    color: var(--color--black);
+    text-decoration: none;
+    text-transform: lowercase;
+    letter-spacing: 1px;
+
+	border: 1px solid  var(--color--black);
+    background-color: var(--color--white);
+
+}
+.woocommerce-cart-empty-container .return-to-shop .button:hover strong{
+    font-weight: 700;
+}
 </style>
 
 <div class="woocommerce-cart-empty-container" style="text-align: center; margin: 50px 0;">
-    <!-- Centered Title -->
-    <h1 style="font-size: 32px; color: #333;"><?php esc_html_e( 'Tu Carrito esta Vacio', 'woocommerce' ); ?></h1>
-    
+
+
     <!-- Centered Image -->
-    <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/BSC_Empty_Cart.jpg' ); ?>" 
+    <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/carita_tristeProductos_PDRN.png' ); ?>" 
          alt="<?php esc_attr_e( 'Empty Cart', 'woocommerce' ); ?>" 
-         style="max-width: 300px; margin: 20px auto; display: block;">
+         style="max-width: 200px; margin: 20px auto; margin-bottom:10px; display: block;">
+
+    <!-- Centered Title -->
+    <h1 style="        margin-bottom: 1.25rem; ">
+        Ohh ... <strong>tu carrito</strong> esta vacío
+    </h1>
+    <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/bsc__waving-line.png' ); ?>" 
+         alt="<?php esc_attr_e( 'Empty Cart', 'woocommerce' ); ?>" 
+         style="max-width: 80px; margin: 0px auto;  display: block;">
+
+    <div style="margin:25px; display:block;"></div>
+
     
     <!-- Return to Shop Button -->
     <?php if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
-        <p class="return-to-shop">
-            <a class="button wc-backward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" 
-               href="<?php echo esc_url( home_url( '/product-category/' ) ); ?>"
-               >
-                <?php esc_html_e( 'Ir a Tienda', 'woocommerce' ); ?>
-            </a>
-        </p>
-    <?php endif; ?>
+    <p class="return-to-shop">
+        <a class="button wc-backward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" 
+           href="<?php echo esc_url( home_url( '/product-category/group-skin-care/sk-rutina/sk-rutina-s1-limpiadores-aceitosos/' ) ); ?>">
+            ¡ Ir a la <strong>tienda</strong> !
+        </a>
+    </p>
+<?php endif; ?>
 </div>
+
