42b13c489a2fe7db61752ed66604850f5cd12a29
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-02-05 14:07:46 -0500

    Update order-received.php

--- woocommerce/checkout/order-received.php
+++ woocommerce/checkout/order-received.php
@@ -20,7 +20,7 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received">
+<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received" style="display: none;">
 	<?php
 	/**
 	 * Filter the message shown after a checkout is complete.
@@ -39,4 +39,65 @@ defined( 'ABSPATH' ) || exit;
 	// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	echo $message;
 	?>
+
+
 </p>
+
+<br>
+<br>
+<br>
+
+<div class="bsc__thankyou-page">
+	<!-- Header Section -->
+	<div class="bsc__thankyou-header">
+		<img class="logo" src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/arcoirisProductosPDRN.png' ); ?>" alt="Thank You Rainbow" class="rainbow-image">
+		<h2 class="thankyou-title">¡ <strong>Gracias</strong> por tu compra !</h2>
+		<img class="wave" src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/bsc__waving-line.png' ); ?>" alt="Thank You Rainbow" class="rainbow-image">
+	</div>
+</div>
+
+<style>
+
+<style>
+.bsc__thankyou-page {
+    display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+}
+.bsc__thankyou-header{
+	text-align: center;
+    width: 500px;
+    margin: 0 auto;
+	font-family: "Helvetica", sans-serif;
+
+	margin-bottom: 60px;
+}
+.bsc__thankyou-header .logo{
+	width: 250px;
+}
+.bsc__thankyou-header .wave{
+    width: 80px;
+}
+
+.bsc__thankyou{
+    margin-bottom: 30px;
+}
+
+.rainbow-image {
+    max-width: 150px;
+    margin-bottom: 15px;
+}
+
+.thankyou-title {
+    font-size: 23px;
+    font-weight: regular;
+	letter-spacing: 2.5px;
+	font-family: "Helvetica", sans-serif;
+}
+.thankyou-title strong{
+	font-weight: bold;
+}
+
+</style>
