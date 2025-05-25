8b63f353c351eaa4ef4af022130c24f76d2a11f7
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-05-25 11:15:32 -0500

    Update form-shipping.php

--- woocommerce/checkout/form-shipping.php
+++ woocommerce/checkout/form-shipping.php
@@ -23,7 +23,9 @@ defined( 'ABSPATH' ) || exit;
 
 		<h3 id="ship-to-different-address">
 			<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
-				<input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" <?php checked( apply_filters( 'woocommerce_ship_to_different_address_checked', 'shipping' === get_option( 'woocommerce_ship_to_destination' ) ? 1 : 0 ), 0 ); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span><?php esc_html_e( '¿Enviar a otra dirección?', 'woocommerce' ); ?></span>
+				<input id="ship-to-different-address-checkbox" 
+				class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox"  style=" text-transform: none;"
+				<?php checked( apply_filters( 'woocommerce_ship_to_different_address_checked', 'shipping' === get_option( 'woocommerce_ship_to_destination' ) ? 1 : 0 ), 0 ); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span style=" text-transform: none;"><?php esc_html_e( '¿Enviar a otra dirección?', 'woocommerce' ); ?></span>
 			</label>
 		</h3>
 
